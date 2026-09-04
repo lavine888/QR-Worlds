49b4a05b66f36d29bf0a25150a8d4b149fa2f153
LavineX <dbest72500@gmail.com>
2026-09-04 20:19:59 +0800

    refactor(v7): route main canvas through raw WebGPU

--- src/scene/WorldCanvas.tsx
+++ src/scene/WorldCanvas.tsx
@@ -1,7 +1,5 @@
-import { Canvas } from '@react-three/fiber';
-import * as THREE from 'three';
 import type { QRMatrix } from '../qr/generateQR';
-import { ReferenceVoxelWorldRefined } from './ReferenceVoxelWorldRefined';
+import { WebGPUWorld } from '../webgpu/WebGPUWorld';
 import type { WorldTheme } from './themes';
 
 type WorldCanvasProps = {
@@ -15,23 +13,10 @@ type WorldCanvasProps = {
 
 export function WorldCanvas({ matrix, scanMode, onCanvasReady }: WorldCanvasProps) {
   return (
-    <Canvas
-      dpr={[1.25, 1.85]}
-      camera={{ position: [0, 0, 2], near: 0.1, far: 10 }}
-      gl={{
-        antialias: true,
-        alpha: false,
-        powerPreference: 'default',
-        preserveDrawingBuffer: true,
-      }}
-      onCreated={({ gl }) => {
-        gl.setClearColor('#f7f7f7', 1);
-        gl.toneMapping = THREE.NoToneMapping;
-        gl.outputColorSpace = THREE.SRGBColorSpace;
-        onCanvasReady?.(gl.domElement);
-      }}
-    >
-      <ReferenceVoxelWorldRefined matrix={matrix} scanMode={scanMode} />
-    </Canvas>
+    <WebGPUWorld
+      matrix={matrix}
+      scanMode={scanMode}
+      onCanvasReady={onCanvasReady}
+    />
   );
 }
