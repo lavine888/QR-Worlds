04cbc7b76924867bc2c495d11c086ae542247c6b
LavineX <dbest72500@gmail.com>
2026-09-04 20:21:48 +0800

    refactor(v7): remove legacy scene props from WebGPU canvas

--- src/scene/WorldCanvas.tsx
+++ src/scene/WorldCanvas.tsx
@@ -1,12 +1,8 @@
 import type { QRMatrix } from '../qr/generateQR';
 import { WebGPUWorld } from '../webgpu/WebGPUWorld';
-import type { WorldTheme } from './themes';
 
 type WorldCanvasProps = {
   matrix: QRMatrix;
-  theme: WorldTheme;
-  seedText: string;
-  worldSeed: number;
   scanMode: boolean;
   onCanvasReady?: (canvas: HTMLCanvasElement) => void;
 };
