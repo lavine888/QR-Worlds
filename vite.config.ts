9dca0711f3d11ae974fc42947c167eabbff03807
LavineX <dbest72500@gmail.com>
2026-09-09 21:03:10 +0800

    build(v9): alias React Native to web runtime

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: './',
   plugins: [react()],
+  resolve: {
+    alias: {
+      'react-native': 'react-native-web',
+    },
+  },
+  optimizeDeps: {
+    include: ['react-native-web', 'react-native-webgpu'],
+  },
   css: {
     postcss: { plugins: [] },
   },
